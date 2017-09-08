fe0c46a1ddd8c8b1b30f91d199e8b7ed8f4b800d
Saheel Wagh <saheelwagh@gmail.com>
2017-09-08 08:20:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ function happyHolidaysTo(person){
   return `Happy holidays, ${person}!`;
 }
 function happyHolidayTo(holiday,person){
-  return `Happy ${holiday}, ${person}`;
+  return `Happy ${holiday}, ${person}!`;
+}
+function  holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
 }
